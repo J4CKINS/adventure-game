bd273af24c9e1c4325ecf793a26db24e62a1ff89
J4CKINS <50251721+J4CKINS@users.noreply.github.com>
2021-03-16 14:34:52 +0000

    implemented menu screen

---
+++ lib/screens/menu.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class MenuScreen extends StatelessWidget {
+
+  String _text = "Welcome to the Adventure Game\n\nYou will have to choose one of two answers on each question, there are different routes.";
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child:Text("Adventure Game", style: TextStyle(fontSize: 30),))),
+      body: Center(
+        child: Container(
+          child: Column(
+            children: [
+              Padding(padding: EdgeInsets.only(top:30)),
+              Text(_text, style: TextStyle(fontSize: 30), textAlign: TextAlign.center,),
+              Padding(padding: EdgeInsets.only(top:50)),
+              ElevatedButton(autofocus: false, clipBehavior: Clip.none, child: Padding(padding: EdgeInsets.all(10), child: Text("Start", style:TextStyle(fontSize: 20, color: Colors.white)),),
+              onPressed: (){})
+          ],),
+        )
+      ),
+    );
+  }
+}
