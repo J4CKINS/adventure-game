77f33721c69d52630267b8760285d069fc41e8c2
J4CKINS <50251721+J4CKINS@users.noreply.github.com>
2021-03-23 14:28:23 +0000

    created ending screen class

---
+++ lib/screens/ending_screen.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class EndingScreen extends StatelessWidget {
+
+  final String text;
+  final String medal;
+
+  EndingScreen({Key key, this.text, this.medal}):super(key:key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Center(child:Text("Adventure Game", style: TextStyle(fontSize: 30),))),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(10, 0, 10, 10),
+        child: Column(
+          children: [
+
+            if(medal != null)
+            Image(image: AssetImage(medal), width: 250, height:250),
+
+            Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              
+              Text(text),
+
+              Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly,children: [
+                ElevatedButton(child: Padding(padding: EdgeInsets.all(10), child: Text("Share")), onPressed: (){
+
+                }),
+
+                ElevatedButton(child: Padding(padding: EdgeInsets.all(10), child: Text("Retry")), onPressed: (){
+
+                }),
+
+                ElevatedButton(child: Padding(padding: EdgeInsets.all(10), child: Text("Menu")), onPressed: (){
+
+                }),
+              ])
+            ],
+            ),
+          ],
+        ),
+      )
+    );
+  }
+}
