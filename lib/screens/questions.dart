2bc9c162731d9e329977f909934b955cba484ad1
J4CKINS <electronicjack4@gmail.com>
2021-03-26 14:32:21 +0000

    added questions

--- lib/screens/questions.dart
+++ lib/screens/questions.dart
@@ -1,15 +1,17 @@
+import 'package:adventureapp/screens/ending_screen.dart';
 import 'package:flutter/material.dart';
 import '../classes/question.dart';
+import '../classes/game_text.dart';
 
 class Question1 extends QuestionState {
-  Question1():super(text:"You find yourself lost within a jungle, it\'s turning dark, what should you do?", images:["sleeping.png", "running_person.png"]);
+  Question1():super(text:GameText.question1, images:["sleeping.png", "running_person.png"]);
 
   @override
   Widget buildControls() {
     return Column(children:[
       ElevatedButton(autofocus: false, clipBehavior: Clip.none, child: Padding(padding: EdgeInsets.all(10), child: Text("Sleep", style:TextStyle(fontSize: 20, color: Colors.white)),),
       onPressed: (){
-        choice = Question3();
+        choice = Question(state: Question3());
       }),
 
       Padding(padding: EdgeInsets.only(top:10),),
@@ -17,7 +19,7 @@ class Question1 extends QuestionState {
       ElevatedButton(autofocus: false, clipBehavior: Clip.none, child: Padding(padding: EdgeInsets.all(10), child: Text("Get Moving", style:TextStyle(fontSize: 20, color: Colors.white)),),
       onPressed: (){
         setState(() {
-          choice = Question2();
+          choice = Question(state: Question2());
         });
       }),
 
@@ -26,7 +28,7 @@ class Question1 extends QuestionState {
 }
 
 class Question2 extends QuestionState {
-  Question2():super(text:"You have decided to get moving, you stumble across a calm river, should you make a boat or follow the river by foot", images: []);
+  Question2():super(text:GameText.question2, images: []);
   bool checkboxChoice = false;
   @override
   Widget buildControls() {
@@ -38,8 +40,8 @@ class Question2 extends QuestionState {
           onChanged: (value){
             setState(() {
               checkboxChoice = !checkboxChoice;
-              if (checkboxChoice){ choice = Question4(); }
-              else {} //TODO route to question 5
+              if (checkboxChoice){ choice = Question(state: Question4(),); }
+              else {choice = Question(state: Question5(),);}
             });
           },
         ),
@@ -52,8 +54,8 @@ class Question2 extends QuestionState {
           onChanged: (value){
             setState(() {
               checkboxChoice = !checkboxChoice;
-              if (checkboxChoice){ choice = Question4(); }
-              else {} //TODO route to question 5
+              if (checkboxChoice){ choice = Question(state: Question4(),); }
+              else {choice = Question(state: Question5(),);}
             });
           },
         ),
@@ -64,9 +66,9 @@ class Question2 extends QuestionState {
 
 class Question3 extends QuestionState {
   int radioChoice;
-  List<QuestionState> choices = [Question1(), Question2()];
+  List<QuestionState> choices = [Question6(), Question5()];
 
-  Question3():super(text:"You find a stable tree and a sheltered area on the ground, where do you sleep?", images: []);
+  Question3():super(text:GameText.question3, images: []);
 
   @override
   Widget buildControls() {
@@ -79,7 +81,7 @@ class Question3 extends QuestionState {
           onChanged: (value) {
             setState(() {
               radioChoice = value;
-              choice = choices[value];
+              choice = Question(state: choices[value],);
             });
           },
         ),
@@ -93,7 +95,7 @@ class Question3 extends QuestionState {
           onChanged: (value) {
             setState(() {
               radioChoice = value;
-              choice = choices[value];
+              choice = Question(state: choices[value],);
             });
           },
         ),
@@ -103,13 +105,74 @@ class Question3 extends QuestionState {
 }
 
 class Question4 extends QuestionState {
-  Question4():super(text: "You have chosen to make a boat, you find pieces of long bamboo and cut them up, should you make a thin or wide boat?\n(Press the image)", images:[]);
+  Question4():super(text: GameText.question4, images:[]);
 
   @override
   Widget buildControls() {
     return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
-      ElevatedButton(onPressed: (){}, child: Image(image: AssetImage('small_raft.jpg'), width: 150, height: 150,)),
-      ElevatedButton(onPressed: (){}, child: Image(image: AssetImage('large_raft.jpg'), width: 150, height: 150,))
+      ElevatedButton(onPressed: (){ navigateTo(EndingScreen(text: GameText.ending2, medal: "bronze_medal.png",)); }, child: Image(image: AssetImage('small_raft.jpg'), width: 150, height: 150,)),
+      ElevatedButton(onPressed: (){ navigateTo(EndingScreen(text: GameText.ending1)); }, child: Image(image: AssetImage('large_raft.jpg'), width: 150, height: 150,))
     ],);
+  }
+}
+
+class Question5 extends QuestionState {
+  double sliderValue = .5;
+  List<EndingScreen> choices = [
+    EndingScreen(text: GameText.ending3,medal: "silver_medal.png"),
+    EndingScreen(text: GameText.ending3,medal: "bronze_medal.png"),
+  ];
+
+  Question5():super(text:GameText.question5, images:["running_person.png", "fighting_person.png"]);
+
+  @override
+  Widget buildControls() {
+    return Row(mainAxisAlignment: MainAxisAlignment.spaceEvenly, children: [
+      Text("Run", textAlign: TextAlign.center,),
+      Slider(
+        value: sliderValue,
+        min:0,
+        max:1,
+        divisions: 2,
+        onChanged: (double value) {
+          setState(() {
+            sliderValue = value;
+            if (value != 0.5) {
+            choice = choices[value.toInt()];
+            }
+          });
+          print(value);
+        },
+      ),
+      Text("Fight", textAlign: TextAlign.center)
+    ],);
+  }
+}
+
+class Question6 extends QuestionState {
+  bool switchValue = false;
+
+  Question6():super(text:GameText.question6, images:["torch.jpeg"]);
+
+  @override
+  Widget buildControls() {
+    return Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+      Text("Off", textAlign: TextAlign.center),
+    
+      Switch(
+        value: switchValue,
+        onChanged: (bool value) {
+          setState(() {
+            switchValue = value;
+            if (value) {
+              choice = EndingScreen(text: GameText.ending4, medal:"gold_medal.png");
+            }else {
+              choice = EndingScreen(text: GameText.ending3, medal:"silver_medal.png");
+            }
+          });
+        },
+      ),
+      Text("On", textAlign: TextAlign.center),
+    ]);
   }
 }
