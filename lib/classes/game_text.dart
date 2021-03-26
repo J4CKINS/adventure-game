3bd3587581d837fa394936f9cf19dad2a6cbb025
J4CKINS <electronicjack4@gmail.com>
2021-03-26 14:30:59 +0000

    created game text class

---
+++ lib/classes/game_text.dart
@@ -0,0 +1,26 @@
+class GameText {
+
+  static String question1 = "You find yourself lost within a jungle, it\'s turning dark, what should you do?";
+  static String question2 = "You have decided to get moving, you stumble across a calm river, should you make a boat or follow the river by foot";
+  static String question3 = "You find a stable tree and a sheltered area on the ground, where do you sleep?";
+  static String question4 = "You have chosen to make a boat, you find pieces of long bamboo and cut them up, should you make a thin or wide boat?\n(Press the image)";
+  static String question5 = "As you are walking around you encounter a mysterious animal, you cannot tell how dangerous this animal is, it seems to be a large animal.\nWhat should you do?";
+  static String question6 = "You have created a strong and sturdy sleeping area up in the tree by connecting two branches together and weaving a strong thin vine around. Should you keep your torch on or off overnight?";
+
+  //endings
+  static String ending1 = "You didn't score enough points for a medal, try again to get a medal!" +
+  "\nScore: 25" +
+  "\n\nMaking a wide boat means it can turn easier losing control of the boat and then capsizing, your character was flung off and broke their ankle on a rock within the river.";
+
+  static String ending2 = "You can do better!" +
+  "\nScore: 50" +
+  "\n\n You chose some correct routes however this isn't  the best route, you was hurt throughout the route.";
+
+  static String ending3 = "Nearly there!" +
+  "\nScore: 75" +
+  "\n\n You chose some correct routes however this isn't  the best route, you was hurt throughout the route.";
+
+  static String ending4 = "Welldone!" +
+  "\nScore: 100" +
+  "\n\nYou decided to sleep with your torch on, therefor mosquitos wouldn't attack, you wake up in the morning with no harm done to you and you carry on moving in the day light...";
+}
